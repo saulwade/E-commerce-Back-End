5948cbf9a993086715b96e7510addaf53393fedd
saulwade <saulwade29@gmail.com>
2023-03-09 10:43:32 -0600

    added the producttag code

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,19 +1,37 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
+    modelName: "product_tag",
     freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+    underscored: true
   }
 );
 
